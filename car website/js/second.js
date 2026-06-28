fa91df02b326a6399f490949ed121af64ba9835f
vrajshah-alt <vs3187871@gmail.com>
2026-06-28 18:12:58 +0530

    Delete car website directory

--- car website/js/second.js
+++
@@ -1,9 +0,0 @@
-
-  $(document).ready(function () {
-    $(".title").animatedHeadline({
-        animationType: "clip",
-        animationDelay: 2000,
-        revealDuration: 800,
-        revealAnimationDelay: 2000,
-    });
-  });
